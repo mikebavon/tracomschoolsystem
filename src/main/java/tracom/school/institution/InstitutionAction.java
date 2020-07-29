d178462256a82f02b6dc29d9de5a4d50c30023c6
Omulosi <mulongojohnpaul@gmail.com>
2020-07-29 15:46:44 +0300

    Fix institution to get data from DB

--- src/main/java/tracom/school/institution/InstitutionAction.java
+++ src/main/java/tracom/school/institution/InstitutionAction.java
@@ -8,16 +8,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 import java.sql.*;
 
 
 @WebServlet(urlPatterns = {"/institutions"})
 public class InstitutionAction extends HttpServlet {
-    List<Institution> institutions = new ArrayList<>();
 
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String name = request.getParameter("name");
@@ -25,45 +22,59 @@ public class InstitutionAction extends HttpServlet {
         String location = request.getParameter("location");
         String type = request.getParameter("type");
 
-
-        /*
         try {
             Database DB = new Database("jdbc:mysql://localhost:3306/", "shule_yetu","root", "desiderata");
             Connection conn = DB.connect();
-            Statement stmt = conn.createStatement();
-            stmt.executeQuery("insert into institutions (name, address, location, type) values("
-                    + ",'" + name + "','" + address + "','" + location + "','" + type +
-                    "')");
+            String sql = "insert into institutions set name=?, address=?, location=?, type =?";
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setString(1, name);
+            stmt.setString(2, address);
+            stmt.setString(3, location);
+            stmt.setString(4, type);
+            stmt.executeUpdate();
+
             System.out.println("saved to DB");
         } catch (SQLException e) {
             System.err.println(e);
         }
-        */
-
-        institutions.add(new Institution(
-              (new Random()).nextInt(1000), name, address, location, type));
-
-//        Database database = new Database("jdbc:mysql://localhost:3306/", "shule_yetu","root", "");
-//        database.executeQuery("insert into institutions (name, address, location, type) values("
-//                + ",'" + name + "','" + address + "','" + location + "','" + type +
-//                "')");
 
         response.getWriter().println("Saved");
 
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-      /* Get data from the database */
+        List<Institution> institutions = new ArrayList<>();
 
-//        Database database = new Database("jdbc:mysql://localhost:3306/", "shule_yetu","root", "");
-//        database.executeQuery("select * from shule_yetu.institutions")
+        ResultSet result = null;
+        try {
+            Database DB = new Database("jdbc:mysql://localhost:3306/", "shule_yetu","root", "desiderata");
+            Connection conn = DB.connect();
+            Statement stmt = conn.createStatement();
+            result = stmt.executeQuery("select * from shule_yetu.institutions");
+            System.out.println(result);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
-      /*
-        institutions.add(new Institution(1L, "ITMO1", "1523 HK", "Hong Kong", "University"));
-        institutions.add(new Institution(2L, "ITMO2", "1523 HK", "Hong Kong", "University"));
-        institutions.add(new Institution(3L, "ITMO3", "1523 HK", "Hong Kong", "University"));
-        institutions.add(new Institution(4L, "ITMO4", "1523 HK", "Hong Kong", "University"));
-        */
+        if (result != null) {
+          System.out.println("Result is not null...");
+          try {
+            while (result.next()) {
+              result.getString(2);
+              institutions.add(
+                  new Institution(
+                    result.getInt("id"),
+                    result.getString("name"),
+                    result.getString("address"),
+                    result.getString("location"),
+                    result.getString("type")
+                    )
+                  );
+            }
+          } catch (SQLException err) {
+              err.printStackTrace();
+          }
+        }
 
         ObjectMapper json = new ObjectMapper();
         response.getWriter().println(json.writeValueAsString(institutions));
