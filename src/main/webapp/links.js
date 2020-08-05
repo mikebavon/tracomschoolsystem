576dc5368e6c1f90d403ba3dcd255c72e911082a
rufusy <idachirufus@gmail.com>
2020-08-05 21:14:44 +0300

    add links to users interface

--- src/main/webapp/links.js
+++ src/main/webapp/links.js
@@ -5,5 +5,6 @@
     TracomAcademy.JsLoader.registerLinkHandler('topnav-students', 'student/student', 'renderStudents');
     TracomAcademy.JsLoader.registerLinkHandler('topnav-tutors', 'tutor/tutor', 'renderTutors');
     TracomAcademy.JsLoader.registerLinkHandler('topnav-departments', 'departments/departments', 'renderDepartments');
-       TracomAcademy.JsLoader.registerLinkHandler('topnav-courses', 'courses/courses', 'renderCourses');
+    TracomAcademy.JsLoader.registerLinkHandler('topnav-courses', 'courses/courses', 'renderCourses');
+    TracomAcademy.JsLoader.registerLinkHandler('topnav-users', 'users/users', 'renderUsers');
 })();
