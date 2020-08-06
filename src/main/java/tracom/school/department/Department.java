2ab884826fab80c9059451e261450bbe836c858a
ngechu <ngechu08@gmail.com>
2020-08-06 10:20:29 +0300

    modify department structure

--- src/main/java/tracom/school/department/Department.java
+++ src/main/java/tracom/school/department/Department.java
@@ -1,50 +1,54 @@
 package tracom.school.department;
 
 public class Department {
-
-        private int id;
-
-        private String dpname;
-
-        private String headofde;
-
-
-    public Department(int id, String dpname, String headofde){
-        this.id = id;
-        this.dpname = dpname;
-        this.headofde = headofde;
-
-    }
-
-    public String getHeadofde() {
-        return headofde;
-    }
-
-    public void setHeadofde(String headofde) {
-        this.headofde = headofde;
-    }
-
-
-
-        public int getId() {
-            return id;
-        }
-
-        public void setId(int id) {
-            this.id = id;
-        }
-
-        public String getDpname() {
-            return dpname;
-        }
-
-        public void setDpname(String dpname) {
-            this.dpname = dpname;
-        }
+private int id;
+private String dpname;
+private String dpcode;
 
 
 
 
-    }
+public Department(){}
+
+public Department(int id, String dpname, String dpcode){
+    this.id = id;
+	this.dpname = dpname;
+	this.dpcode = dpcode;
+	
+    
+}
 
 
+
+
+public String getDpcode() {
+	return dpcode;
+}
+
+public void setDpcode(String dpcode) {
+	this.dpcode = dpcode;
+}
+
+public String getDpname() {
+	return dpname;
+}
+
+public void setDpname(String dpname) {
+	this.dpname = dpname;
+}
+
+public int getId() {
+	return id;
+}
+
+public void setId(int id) {
+	this.id = id;
+}
+
+
+
+
+
+}
+
+    
