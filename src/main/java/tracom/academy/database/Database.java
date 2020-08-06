7485ae9bb4ed6780e1959fe504b5e6aba8300a63
Bettykirii <warwarekirii@gmail.com>
2020-08-06 11:20:54 +0300

    add this keyword

--- src/main/java/tracom/academy/database/Database.java
+++ src/main/java/tracom/academy/database/Database.java
@@ -194,7 +194,7 @@ public class Database {
     public Connection connect() {
         Connection conn = null;
         try {
-            conn = DriverManager.getConnection(url+database, userName, password);
+            conn = DriverManager.getConnection(this.url+this.database, this.userName, this.password);
             System.out.println("Connected to the DB server successfully.");
         } catch (SQLException e) {
             System.out.println(e.getMessage());
