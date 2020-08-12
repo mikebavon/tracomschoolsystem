8e0db7f2474fb85cc523d7effb89b7ba96e0d602
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2020-08-12 16:11:27 +0300

    convert Student to entity class

--- src/main/java/tracom/school/student/Student.java
+++ src/main/java/tracom/school/student/Student.java
@@ -1,18 +1,27 @@
 package tracom.school.student;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="students")
 public class Student {
 
+    @Id
     private int id;
+
+    @Column
     private String name;
+
+    @Column
     private String email;
 
     public Student() { }
 
-    public Student(int id, String name, String email) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-    }
+//    public Student(int id, String name, String email) {
+//        this.id = id;
+//        this.name = name;
+//        this.email = email;
+//    }
 
     public int getId() {
         return id;
@@ -26,7 +35,11 @@ public class Student {
     public void setName(String name) {
         this.name = name;
     }
-    public void setEmail(String email) { this.email = email;}
-    public String getEmail() { return this.email; }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getEmail() {
+        return this.email;
+    }
 
 }
